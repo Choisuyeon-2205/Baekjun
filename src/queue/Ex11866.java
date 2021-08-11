562a91b164123c97117aee411604388f49f70b27
suyeon <suyeon2205@naver.com>
2021-08-11 23:45:32 +0900

    21-08-11

--- src/queue/Ex11866.java
+++ src/queue/Ex11866.java
@@ -8,19 +8,31 @@ public class Ex11866 {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
+		StringBuilder sb= new StringBuilder();
+		sb.append("<");
 		int n= sc.nextInt(); //n 입력받기
 		int k= sc.nextInt(); //k 입력받기
 		
 		Queue<Integer> que= new LinkedList<Integer>();
-		int idx= k;
-		while(que.size()<n) {
-			que.add(idx); //3 6 2
+		
+		for(int i=1;i<=n;i++) {
+			que.add(i); //1-n 까지 수를 큐에 삽입
+		}
+		
+		while(que.size() > 1) {
 			
-			int i=0;
-			while(i<k) {
+			for(int i = 0; i < k - 1; i++) {
+				int val = que.poll();
+				que.offer(val);
 			}
 			
+			sb.append(que.poll()).append(", ");
 		}
+ 
+		// 마지막 원소 출력한 뒤 > 도 붙여준다.
+		sb.append(que.poll()).append(">");
+		System.out.println(sb);
+		
 	}
 
 }
