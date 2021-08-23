317c751012c66c803c3313e2e55d75464bfb7ad6
suyeon <suyeon2205@naver.com>
2021-08-23 22:00:22 +0900

    21-08-23

---
+++ src/dynamic/Ex1932.java
@@ -0,0 +1,41 @@
+package dynamic;
+
+import java.util.Scanner;
+
+public class Ex1932 {
+	
+	static Integer[][] sum;
+	static int[][] num;
+	static int n;
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		n= sc.nextInt();
+		sum= new Integer[n][n];
+		num= new int[n][n];
+		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<i+1;j++) {
+				num[i][j]= sc.nextInt();
+			}
+		}
+		
+		for(int i=0;i<n;i++) {
+			sum[n-1][i]= num[n-1][i];
+		}
+
+		System.out.println(max_sum(0,0));
+	}
+
+        static int max_sum(int depth, int index) {
+			if(depth==n-1) return sum[depth][index];
+			
+			if(sum[depth][index]==null) {
+				sum[depth][index]= Math.max(max_sum(depth+1, index), max_sum(depth+1, index+1))+ num[depth][index];
+			}
+			
+        	return sum[depth][index];
+		
+	}
+
+}
