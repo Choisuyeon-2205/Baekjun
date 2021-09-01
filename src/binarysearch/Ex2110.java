19e82396cac3f4644b71a9b059adbf959c9aad42
suyeon <suyeon2205@naver.com>
2021-09-01 22:49:47 +0900

    21-09-01

---
+++ src/binarysearch/Ex2110.java
@@ -0,0 +1,21 @@
+package binarysearch;
+
+import java.util.Scanner;
+
+public class Ex2110 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		int n= sc.nextInt();
+		int c= sc.nextInt();
+		
+		int[] home= new int[n];
+		for(int i=0;i<n;i++) {
+			home[i]= sc.nextInt();
+		}
+		
+		
+
+	}
+
+}
