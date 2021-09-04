b80b89d739d7915d0df6e1e3b1271b1e87d127a9
suyeon <suyeon2205@naver.com>
2021-09-04 13:11:20 +0900

    21-09-04

--- src/cote/Ex3_1.java
+++ src/cote/Ex3_1.java
@@ -8,12 +8,14 @@ public class Ex3_1 {
 		long answer= 0;
 		long[][] arr= new long[2][4];
 		for(long[] i:arr) Arrays.fill(i,1);
-		
+	
 		for(int i=1;i<2;i++) {
 			for(int j=1;j<4;j++) {
 				arr[i][j]= arr[i-1][j]+arr[i][j-1];
 			}
 		}
+		
+		Math.abs(answer);
 		
         answer= arr[1][3];
         System.out.println(answer);
